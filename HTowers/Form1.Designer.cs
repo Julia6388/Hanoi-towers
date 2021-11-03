b36bfdf309a51bd49694bd45eb8a9c3eb2f78be7
Juju6388 <ulia210700@mail.ru>
2021-11-03 23:37:16 +0300

    Головоломка Ханойские башни

---
+++ HTowers/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace HTowers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 282);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Начальная позиция";
+            this.groupBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.groupBox1_DragDrop);
+            this.groupBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.groupBox1_DragEnter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(227, 49);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 282);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.groupBox1_DragDrop);
+            this.groupBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.groupBox1_DragEnter);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(447, 49);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 282);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.groupBox1_DragDrop);
+            this.groupBox3.DragEnter += new System.Windows.Forms.DragEventHandler(this.groupBox1_DragEnter);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Сброс";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(276, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(276, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Задача решена!";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(447, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Показать решение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ханойские башни";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HTowers/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTowers
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            groupBox1.AllowDrop = groupBox2.AllowDrop = groupBox3.AllowDrop = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int n = (int)numericUpDown1.Value,
+                w = groupBox1.Width,
+                h = groupBox1.Height;
+            for (int i=0;i<n;i++)
+            {
+                Label lb = new Label();                
+                lb.BorderStyle = BorderStyle.FixedSingle;
+                lb.Size = new Size((w - 10) * (n - i) / n, (h - 15) / n);
+                lb.Location = new Point((w - lb.Width) / 2, h - 2 - (i + 1) * lb.Height);
+                lb.BackColor = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+                groupBox1.Controls.Add(lb);
+                lb.MouseDown += label_MouseDown;
+            }
+            count = 0;
+            minCount = (int)Math.Round(Math.Pow(2, n)) - 1;
+            Info();
+            label2.Visible = false;
+        }
+
+
+        private void label_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!button1.Enabled)
+                return;
+            if (e.Button == MouseButtons.Left)
+                DoDragDrop(sender, DragDropEffects.Move);
+        }
+        //Перемещение блока
+        private void label_Move(Label lb, GroupBox gb)
+        {
+            lb.Parent = gb;
+            lb.Top = gb.Height - 2 - gb.Controls.Count * lb.Height; ;
+            count++;
+            Info();
+            if (groupBox2.Controls.Count == numericUpDown1.Value || groupBox3.Controls.Count == numericUpDown1.Value)
+                label2.Visible = true;
+        }
+        // Удаление башни для перерисовки
+        private void label_Dispose (GroupBox gb)
+        {
+            for (int i = gb.Controls.Count - 1; i >= 0; i--)
+                gb.Controls[i].Dispose();
+        }
+        //запуск формы заново при изменении количества блоков
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            label_Dispose (groupBox1);
+            label_Dispose(groupBox2);
+            label_Dispose(groupBox3);
+            Form1_Load(this, null);
+        }
+        //Перетаскивание на другой бокс + нельзя перетаскивать нижние блоки
+        // + нельзя ставить маленький на большой
+        private void groupBox1_DragEnter (object sender,DragEventArgs e)
+        {
+            Label lb = e.Data.GetData(typeof(Label)) as Label;
+            int k = int.MaxValue;
+            GroupBox gb = sender as GroupBox;
+            if(gb.Controls.Count>0)
+                k = gb.Controls[gb.Controls.Count - 1].Width;
+            if (lb.Parent.Controls[lb.Parent.Controls.Count-1] != lb || lb.Width > k)
+                e.Effect = DragDropEffects.None;
+            else  e.Effect = DragDropEffects.Move;
+            if (label2.Visible)
+            {
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+        }
+        //пеирерисовка в другой блок
+        private void groupBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            Label lb = e.Data.GetData (typeof(Label)) as Label;
+            GroupBox gb = sender as GroupBox;
+            if (gb == lb.Parent)
+                return;
+            label_Move(lb, gb);
+        }
+        private int count;
+        private int minCount;
+        // Подсчет количества шагов
+        private void Info()
+        {
+            label1.Text = string.Format("Число ходов: {0} ({1})", count, minCount);
+        }
+        private void Step (int n, GroupBox src,GroupBox dst, GroupBox tmp)
+        {
+            if (n == 0)
+                return;
+            Step(n - 1, src, tmp, dst);
+            if (button1.Enabled)
+                return;
+            label_Move(src.Controls[src.Controls.Count - 1] as Label, dst);
+            Application.DoEvents(); ;
+            System.Threading.Thread.Sleep(1500 / ((int)numericUpDown1.Value) - 1);
+            Step(n - 1, tmp, dst, src);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Enabled = button1.Enabled = !button1.Enabled;
+            if(!button1.Enabled)
+            {
+                if (groupBox1.Controls.Count != numericUpDown1.Value)
+                    NumericUpDown1_ValueChanged(null, null);
+                Step((int)numericUpDown1.Value, groupBox1, groupBox3, groupBox2);
+                numericUpDown1.Enabled = button1.Enabled = true;
+            }
+        }
+        private void Form1_Form1Cloused (object senger, FormClosedEventArgs e)
+        {
+            button1.Enabled = true;
+        }
+    }
+}
